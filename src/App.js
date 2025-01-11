e052777ea472cc348b7e5ae07995d731f6614b09
steve <steven.bergsman@gmail.com>
2025-01-11 08:14:57 -0600

    330: App Init part 2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import CarForm from "./components/CarForm";
+import CarList from "./components/CarList";
+import CarSearch from "./components/CarSearch";
+import CarValue from "./components/CarValue";
 
 function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React Starter Project</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.js</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
-}
+    return <div>
+        <CarForm/>
+        <CarSearch/>
+        <CarList/>
+        <CarValue/>
+    </div>
+};
 
 export default App;
---
+++ src/components/CarForm.js
@@ -0,0 +1,7 @@
+function CarForm() {
+    return <div>
+        Car Form...
+    </div>
+}
+
+export default CarForm;
